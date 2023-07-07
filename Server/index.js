c1782e91c4f1421e76f1f9c6a49d7da190b4da6e
Chirag <chiragsaini6405@gmail.com>
2023-07-08 02:40:59 +0530

    commit

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://door-step.vercel.app/",
+    origin: "http://localhost:3000/",
     methods: ["POST", "GET"],
     credentials: true,
   })
