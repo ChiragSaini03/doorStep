50a124a4edc0f6af7d0c46882890a987dc77669a
Chirag <chiragsaini6405@gmail.com>
2023-07-08 02:58:52 +0530

    modified

--- frontend/src/Client/components/Vendors_Components/Vendor_Home.jsx
+++ frontend/src/Client/components/Vendors_Components/Vendor_Home.jsx
@@ -163,7 +163,7 @@ const Vendor_Home = (props) => {
               </div>
               <div>
                   <label for="stock" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stock</label>
-                  <input type="url" id="stock" value={stock} onChange={stock_handler} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="flowbite.com" required></input>
+                  <input type="number" id="stock" value={stock} onChange={stock_handler} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1000" required></input>
               </div>
               <div>
                   <label for="discount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Discont (%) </label>
