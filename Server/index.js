43d96134c4dfff678ffc5f1e3f0ee9a6aada828a
Chirag <chiragsaini6405@gmail.com>
2023-07-08 02:45:03 +0530

    origin of cors

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000/",
+    origin: "https://door-step-frontend.vercel.app/",
     methods: ["POST", "GET"],
     credentials: true,
   })
