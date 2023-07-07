0535c74a9cae080642ff1b47416ecb4984f1ceec
Chirag <chiragsaini6405@gmail.com>
2023-07-08 02:12:43 +0530

    commit

--- Server/db/login_db.js
+++ Server/db/login_db.js
@@ -1,12 +1,12 @@
-const mongo_conn = require("mongoose");
-mongo_conn.set("strictQuery", true);
-mongo_conn
-  .connect(
-    "mongodb+srv://doorstep_db_rohit:DoorStep123@cluster0.chgewfl.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log("conn is succcessfull");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// const mongo_conn = require("mongoose");
+// mongo_conn.set("strictQuery", true);
+// mongo_conn
+//   .connect(
+//     "mongodb+srv://doorstep_db_rohit:DoorStep123@cluster0.chgewfl.mongodb.net/?retryWrites=true&w=majority"
+//   )
+//   .then(() => {
+//     console.log("conn is succcessfull");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,7 @@ app.use(
 );
 
 const mongo_conn = require("mongoose");
-mongo_conn.set("strictQuery", true);
+// mongo_conn.set("strictQuery", true);
 mongo_conn
   .connect(
     "mongodb+srv://doorstep_db_rohit:DoorStep123@cluster0.chgewfl.mongodb.net/?retryWrites=true&w=majority"
